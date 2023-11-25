63991a9f9e46bffe89a1acf12eb6d13b9b740267
Asanalii <asanaliiospannn@gmail.com>
2023-11-25 17:22:04 +0600

    add: added frist onboarding page, added fonts, images and made styles

--- app/src/main/java/com/example/ayirbasta/MainActivity.kt
+++ app/src/main/java/com/example/ayirbasta/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.ayirbasta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ayirbasta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.progressBar.progress = 30
     }
 }
